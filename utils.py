d37436729d1f572d1ec3c17381c79ff883bf7f7e
LilleBaro <msqure59@outlook.com>
2026-06-13 21:01:37 +0000

    instenciation du model dans model.py

--- utils.py
+++ utils.py
@@ -1,8 +1,4 @@
-from torchvision.datasets import ImageFolder
-from torch.utils.data import DataLoader, Dataset
 import matplotlib.pyplot as plt
-from dataset import TextClassificationDataset
-from collections import Counter
 from sklearn.metrics import accuracy_score, f1_score 
 # %%
 def compute_metrics(preds, labels):
